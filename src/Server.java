e2f88e205450d27ca6e1f201273139187f11f72d
mvrgs <m.vargas@estudiantec.cr>
2023-05-15 00:06:46 -0600

    Login

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,7 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -11,11 +12,17 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
     private static ArbolBinarioBusqueda arbolClientes = new ArbolBinarioBusqueda();
     private static ArbolBinarioBusqueda arbolUsuarios = new ArbolBinarioBusqueda();
     private static ArbolAVL arbolPlatillos = new ArbolAVL();
+    private static ArrayList<String> platillosLista;
+    static Socket clientSocket;
+    static ObjectOutputStream out;
+    static ObjectInputStream in;
+
 
 
     private static void cargarClientes(String nombreArchivo) {
@@ -75,38 +82,95 @@ public class Server {
             for (Object obj : jsonArray) {
                 JSONObject jsonObject = (JSONObject) obj;
                 Platillos platillo = new Platillos();
-                platillo.setNombre((String) jsonObject.get("nombre"));
-                platillo.setCalorias((Integer) jsonObject.get("calorias"));
-                platillo.setTiempo((Integer) jsonObject.get("tiempo"));
-                platillo.setPrecio((Integer) jsonObject.get("precio"));
+                platillo.setNombre((String) jsonObject.get("Nombre"));
+                Long caloLong = (Long) jsonObject.get("Calorias");
+                Integer calorias = caloLong.intValue();
+                platillo.setCalorias(calorias);
+                Long timeLong = (Long) jsonObject.get("Tiempo");
+                Integer tiempo = timeLong.intValue();
+                platillo.setTiempo(tiempo);
+                Long precioLong = (Long) jsonObject.get("Precio");
+                Integer precio = precioLong.intValue();
+                platillo.setPrecio(precio);
                 arbolPlatillos.insert(platillo);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args) throws IOException {
+    private static void cargarListaPlatillos(String nombreArchivo)  {
+        try{
+            JSONParser parser = new JSONParser();
+            FileReader reader = new FileReader(nombreArchivo);
+            JSONArray jsonArray = (JSONArray) parser.parse(reader);
+            platillosLista= new ArrayList<>();
+            for (Object obj : jsonArray){
+                JSONObject jsonObject = (JSONObject) obj;
+                platillosLista.add((String) jsonObject.get("Nombre"));
+                System.out.println(platillosLista);
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                out.writeObject(platillosLista);
+
+
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         cargarClientes("clientes.xml");
         cargarUsuarios("usuarios.xml");
         cargarPlatillos("platillos.json");
-
         ServerSocket serverSocket = new ServerSocket(01234);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        while(true){
+            clientSocket = serverSocket.accept();
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            in = new ObjectInputStream(clientSocket.getInputStream());
 
-        String inputLine;
+            String mensaje = (String) in.readObject();
+
+            if (mensaje.equals("validarLogin")){
+                inicioSesion();
+
+            }
+
+        }
+
+       /*
         while ((inputLine = in.readLine()) != null) {
             System.out.println(inputLine);
 
-            /*
+
             out.println("Echo from server: " + inputLine);
 
              */
         }
-    }
-    private static void validarLogin(){
-        String usernameIn;
-        String passwordIn;
+    private static void inicioSesion() throws IOException, ClassNotFoundException {
+        String usernameIn= (String) in.readObject();
+        String passwordIn = (String) in.readObject();
+
+        System.out.println("Usuario:"+usernameIn);
+        System.out.println("Password:"+passwordIn);
+
+        // Buscar el usuario en ambos árboles
+        Usuario usuario = arbolUsuarios.buscar(usernameIn, passwordIn);
+        if (usuario == null) {
+            usuario = arbolClientes.buscar(usernameIn, passwordIn);
+        }
+
+        // Validar en qué árbol se encuentra el usuario y abrir la ventana correspondiente
+        if (usuario != null) {
+            if (arbolUsuarios.buscar(usernameIn,passwordIn) != null) {
+                out.writeObject("Administrador");
+
+            } else if (arbolClientes.buscar(usernameIn,passwordIn) != null) {
+                out.writeObject("Cliente");
+            }
+        } else {
+            out.writeObject("Incorrectos");
+        }
+
+
     }
 }
