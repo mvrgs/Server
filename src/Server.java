e8025695c6709ed5105bbe2477fa8713b43bf3ba
mvrgs <m.vargas@estudiantec.cr>
2023-05-15 00:47:11 -0600

    agregarAdmi

--- src/Server.java
+++ src/Server.java
@@ -9,6 +9,11 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,6 +27,7 @@ public class Server {
     static Socket clientSocket;
     static ObjectOutputStream out;
     static ObjectInputStream in;
+
 
 
 
@@ -123,29 +129,80 @@ public class Server {
         cargarUsuarios("usuarios.xml");
         cargarPlatillos("platillos.json");
         ServerSocket serverSocket = new ServerSocket(01234);
-        while(true){
+        while (true) {
             clientSocket = serverSocket.accept();
             out = new ObjectOutputStream(clientSocket.getOutputStream());
             in = new ObjectInputStream(clientSocket.getInputStream());
 
             String mensaje = (String) in.readObject();
+            System.out.println(mensaje);
 
-            if (mensaje.equals("validarLogin")){
+            if (mensaje.equals("validarLogin")) {
                 inicioSesion();
-
+            } if (mensaje.equals("agregarAdmin")) {
+                nuevoAdmi();
             }
 
+
+
         }
+    }
 
-       /*
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
+    private static void nuevoAdmi() throws IOException, ClassNotFoundException {
+        String newUsuario = (String) in.readObject();
+        String newPassword = (String) in.readObject();
 
+        Usuario usuario = new Usuario();
+        usuario.setUsername(newUsuario);
+        usuario.setPassword(newPassword);
+        usuario.setRol("Administrador");
+        System.out.println(newUsuario);
+        System.out.println(newPassword);
 
-            out.println("Echo from server: " + inputLine);
-
-             */
+        try {
+            agregarUsuarioAXML("usuarios.xml", usuario);
+            out.writeObject("Administrador agregado con éxito");
+        } catch (Exception e) {
+            out.writeObject("Error al agregar el administrador");
         }
+    }
+
+    public static void agregarUsuarioAXML(String nombreArchivo, Usuario usuario) throws Exception {
+        // Crear una fábrica de constructores de documentos
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+
+        // Leer el archivo XML existente
+        Document doc = docBuilder.parse(new File(nombreArchivo));
+
+        // Obtener la raíz del documento
+        Element raiz = doc.getDocumentElement();
+
+        // Crear un nuevo elemento para el usuario
+        Element nuevoUsuario = doc.createElement("usuario");
+        raiz.appendChild(nuevoUsuario);
+
+        Element username = doc.createElement("username");
+        username.appendChild(doc.createTextNode(usuario.getUsername()));
+        nuevoUsuario.appendChild(username);
+
+        Element password = doc.createElement("password");
+        password.appendChild(doc.createTextNode(usuario.getPassword()));
+        nuevoUsuario.appendChild(password);
+
+        Element rol = doc.createElement("rol");
+        rol.appendChild(doc.createTextNode(usuario.getRol()));
+        nuevoUsuario.appendChild(rol);
+
+        // Guardar los cambios en el archivo XML
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new FileOutputStream(nombreArchivo));
+        transformer.transform(source, result);
+    }
+
     private static void inicioSesion() throws IOException, ClassNotFoundException {
         String usernameIn= (String) in.readObject();
         String passwordIn = (String) in.readObject();
